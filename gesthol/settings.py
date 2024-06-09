01ad032fe893095ee59dc2bb52da6f25a9d16d69
carlichezz <carlosarielsanchez02@gmail.com>
2024-06-09 15:23:38 -0400

    render.2

--- gesthol/settings.py
+++ gesthol/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = 'gesthol.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default= 'postgresql://postgres:mkCCcxAWlzILEqTAfcjCRhSIQGRMaVIT@postgres.railway.internal:5432/railway',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
@@ -122,14 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
